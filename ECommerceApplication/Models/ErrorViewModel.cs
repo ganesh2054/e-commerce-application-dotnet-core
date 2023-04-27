fa44f1aad9f119c84cfa48417f3325bf549735f9
Ganesh Lamichhane <ganeshlamichhane202@gmail.com>
2023-04-27 09:09:15 +0545

    project setup

---
+++ ECommerceApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
